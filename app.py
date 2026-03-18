15a3ecaa26c4d6d65df8598cde8c671dcb75c694
Hugo Miquilino <hugomiquilino@gmail.com>
2026-03-18 16:30:49 -0300

    Implementada função add() responsável pelo método POST para inserção da tarefa.

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+tarefas = []
+
 @app.route("/")
 def home():
-    return 'Ok'
+    return render_template('index.html', tarefas=tarefas)
+
+@app.route("/add", methods=["POST"])
+def add():
+    texto = request.form.get("tarefa")
+    tarefas.append({"texto": texto, "feito": False})
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True)
