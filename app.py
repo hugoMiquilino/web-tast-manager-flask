263684458554b3b18f9a90c647088ad42f884e5b
Hugo Miquilino <hugomiquilino@gmail.com>
2026-03-19 18:18:22 -0300

    Adicionada a estrutura HTML e CSS para a interface do gerenciador de tarefas.

--- app.py
+++ app.py
@@ -4,22 +4,32 @@ app = Flask(__name__)
 
 tarefas = []
 
+
 @app.route("/")
 def home():
-    return render_template('index.html', tarefas=tarefas)
+    return render_template("index.html", tarefas=tarefas)
+
 
 @app.route("/add", methods=["POST"])
 def add():
-    texto = request.form.get("tarefa")
-    tarefas.append({"texto": texto, "feito": False})
+    texto = request.form.get("tarefa", "").strip()
+
+    if texto:
+        tarefas.append({"texto": texto, "feito": False})
+
     return redirect("/")
+
 
 @app.route("/done/<int:id>")
 def done(id):
-    tarefas["id"]["feito"] = True
+    tarefas[id]["feito"] = True
     return redirect("/")
 
 
+@app.route("/delete/<int:id>")
+def delete(id):
+    tarefas.pop(id)
+    return redirect("/")
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+app.run(debug=True)
